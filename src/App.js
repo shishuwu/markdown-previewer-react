f824cc0f2aa3630dd0ae0329daaede6bbb2d7297
shishuwu <shuwu.shi@gmail.com>
2020-02-08 20:32:55 +0800

    samples

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
+// css - 有先后顺序
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+// css - end 
+
 import marked from 'marked';
 
 class Markdown extends React.Component {
   constructor() {
     super();
     this.state = {
-      value: '# hello world'
+      value: '# hello world \n ## hello 2 \n> note \n>> note2 \n\n [link](http://github.com/shishuwu) \n *italic* **bold text** \n\n ``Use `code` in your Markdown file.`` \n ``` inline ```\n ![](https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQUkdXeYPKUxlhVh2WWCxRYdM6VRsR8sV8rfPpLlgUbuIq6kpfI) \n\n `block` \n\n - item1 \n -item2 \n'
     };
   }
 
@@ -17,7 +20,7 @@ class Markdown extends React.Component {
     return { __html: rawMarkup };
   }
 
-  updateMarkStateValue(event) {
+  updateMdStateValue(event) {
     this.setState({ value: event.target.value });
   }
 
@@ -25,10 +28,10 @@ class Markdown extends React.Component {
     return (
       <div className="row">
         <div className="col-md-6">
-          <textarea type="text" className="markdown-input" value={this.state.value} onChange={this.updateMarkStateValue.bind(this)}></textarea>
+          <textarea id="editor" type="text" className="md-input" value={this.state.value} onChange={this.updateMdStateValue.bind(this)}></textarea>
         </div>
         <div className="col-md-6">
-          <div className="markdown-preview" dangerouslySetInnerHTML={this.toPreview(this.state.value)}>
+          <div id="preview" className="md-preview" dangerouslySetInnerHTML={this.toPreview(this.state.value)}>
           </div>
         </div>
       </div>
